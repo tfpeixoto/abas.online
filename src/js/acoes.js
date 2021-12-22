6c5ceff7b5d817e8db2db2b0f5873f2f6ea98a4d
Thiago Peixoto <tfpeixoto@gmail.com>
2021-12-22 14:53:45 -0300

    Incluíndo chave secret da API do Abas

--- src/js/acoes.js
+++ src/js/acoes.js
@@ -104,7 +104,7 @@ function cadastrarLead(event, form) {
   event.preventDefault();
 
   var myHeaders = new Headers();
-  myHeaders.append("Authorization", "Bearer fV/oh7SnZJ4baYPayeQgRO");
+  myHeaders.append("Authorization", "Bearer ${{ secrets.API_ABAS }}");
 
   var raw = new FormData(form)
 
