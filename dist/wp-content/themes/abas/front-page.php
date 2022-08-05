3c58fac263213136c15981328f819a4169a83049
Carlos Eduardo Mayrinck Mendes <carlos.mendes@groupsoftware.com.br>
2022-08-05 14:27:36 -0300

    alteração da exibição dos cards de segmentos na homepage

--- dist/wp-content/themes/abas/front-page.php
+++ dist/wp-content/themes/abas/front-page.php
@@ -44,76 +44,94 @@ require_once("header.php");
     </div>
 
     <div class="row">
-      <div class="col-12 col-md-4 segmentos__card">
-        <a href="segmento/departamento-pessoal" title="Gestão de departamento pessoal">
-          <img src="<?php bloginfo('template_url'); ?>/images/thumbs/departamento-pessoal.jpg" class="img-fluid" alt="Gestão de departamento pessoal" />
+      <div class="col-12 col-md-4">
+        <a href="segmento/departamento-pessoal" title="Gestão de departamento pessoal" class="segmentos__card">
+          <div class="card-head">
+            <img src="<?php bloginfo('template_url'); ?>/images/thumbs/departamento-pessoal.jpg" class="img-fluid" alt="Gestão de departamento pessoal" />
+            <p>Personalize alertas e faça cálculo automático de férias por período de aquisição e substituição de funcionários. Calcule os benefícios: VT, VA e VR.</p>
+          </div>
           <h3>Departamento Pessoal</h3>
-          <p>Personalize alertas e faça cálculo automático de férias por período de aquisição e substituição de funcionários. Calcule os benefícios: VT, VA e VR.</p>
         </a>
       </div>
 
-      <div class="col-12 col-md-4 segmentos__card">
-        <a href="segmento/recursos-humanos" title="Gestão de recursos humanos">
-          <img src="<?php bloginfo('template_url'); ?>/images/thumbs/recursos-humanos.jpg" class="img-fluid" alt="Gestão de recursos humanos" />
+      <div class="col-12 col-md-4">
+        <a href="segmento/recursos-humanos" title="Gestão de recursos humanos" class="segmentos__card">
+          <div class="card-head">
+            <img src="<?php bloginfo('template_url'); ?>/images/thumbs/recursos-humanos.jpg" class="img-fluid" alt="Gestão de recursos humanos" />
+            <p>Facilite a seleção de candidatos: abertura e acompanhamento de vagas e envio automático de e-mails com o status da oportunidade.</p>
+          </div>
           <h3>Recursos Humanos</h3>
-          <p>Facilite a seleção de candidatos: abertura e acompanhamento de vagas e envio automático de e-mails com o status da oportunidade.</p>
         </a>
       </div>
 
-      <div class="col-12 col-md-4 segmentos__card">
-        <a href="fluxo-de-trabalho/gestao-de-compras" title="Gestão de compras e estoque">
-          <img src="<?php bloginfo('template_url'); ?>/images/thumbs/compras-estoque.jpg" class="img-fluid" alt="Gestão de compras e estoque" />
+      <div class="col-12 col-md-4">
+        <a href="fluxo-de-trabalho/gestao-de-compras" title="Gestão de compras e estoque" class="segmentos__card">
+          <div class="card-head">
+            <img src="<?php bloginfo('template_url'); ?>/images/thumbs/compras-estoque.jpg" class="img-fluid" alt="Gestão de compras e estoque" />
+            <p>Gerencie o processo de compras e estoque - desde o envio de e-mails ao fornecedor solicitando cotações até a baixa no estoque.</p>
+          </div>
           <h3>Compras e Estoque</h3>
-          <p>Gerencie o processo de compras e estoque - desde o envio de e-mails ao fornecedor solicitando cotações até a baixa no estoque.</p>
         </a>
       </div>
 
-      <div class="col-12 col-md-4 segmentos__card">
-        <a href="segmento/manutencao" title="Gestão de manutenções">
-          <img src="<?php bloginfo('template_url'); ?>/images/thumbs/manutencao.jpg" class="img-fluid" alt="Gestão de manutenções" />
+      <div class="col-12 col-md-4">
+        <a href="segmento/manutencao" title="Gestão de manutenções" class="segmentos__card">
+          <div class="card-head">
+            <img src="<?php bloginfo('template_url'); ?>/images/thumbs/manutencao.jpg" class="img-fluid" alt="Gestão de manutenções" />
+            <p>Controle as manutenções que possuem periodicidade, receba alertas dos vencimentos e facilite a vistoria de acordo com as normas da ABNT.</p>
+          </div>
           <h3>Manutenção</h3>
-          <p>Controle as manutenções que possuem periodicidade, receba alertas dos vencimentos e facilite a vistoria de acordo com as normas da ABNT.</p>
+
         </a>
       </div>
 
-      <div class="col-12 col-md-4 segmentos__card">
-        <a href="segmento/juridico" title="Gestão de setor jurídico">
-          <img src="<?php bloginfo('template_url'); ?>/images/thumbs/juridico.jpg" class="img-fluid" alt="Gestão de setor jurídico" />
+      <div class="col-12 col-md-4">
+        <a href="segmento/juridico" title="Gestão de setor jurídico" class="segmentos__card">
+          <div class="card-head">
+            <img src="<?php bloginfo('template_url'); ?>/images/thumbs/juridico.jpg" class="img-fluid" alt="Gestão de setor jurídico" />
+            <p>Gerencie as apólices e contratos, faça anotações sobre o andamento de processos judiciais e emita alertas de vencimentos de forma automática.</p>
+          </div>
           <h3>Jurídico</h3>
-          <!-- <p>Rastreie dados pessoais e sensíveis, facilitando a elaboração do DPIA. Monitore os incidentes de segurança e a gestão de solicitação dos titulares.</p> -->
-          <p>Gerencie as apólices e contratos, faça anotações sobre o andamento de processos judiciais e emita alertas de vencimentos de forma automática.</p>
         </a>
       </div>
 
-      <div class="col-12 col-md-4 segmentos__card">
-        <a href="segmento/tecnologia-da-informacao" title="Gestão de demandas de TI">
-          <img src="<?php bloginfo('template_url'); ?>/images/thumbs/tecnologia-informacao.jpg" class="img-fluid" alt="Gestão de demandas de TI" />
+      <div class="col-12 col-md-4">
+        <a href="segmento/tecnologia-da-informacao" title="Gestão de demandas de TI" class="segmentos__card">
+          <div class="card-head">
+            <img src="<?php bloginfo('template_url'); ?>/images/thumbs/tecnologia-informacao.jpg" class="img-fluid" alt="Gestão de demandas de TI" />
+            <p>Crie tarefas e faça a gestão de demandas do time de desenvolvimento. Aloque horas, acompanhe o andamento e envie status das atividades.</p>
+          </div>
           <h3>Tecnologia da Informação</h3>
-          <p>Crie tarefas e faça a gestão de demandas do time de desenvolvimento. Aloque horas, acompanhe o andamento e envie status das atividades.</p>
         </a>
       </div>
 
-      <div class="col-12 col-md-4 segmentos__card">
-        <a href="segmento/marketing-e-comercial" title="Gestão de Marketing">
-          <img src="<?php bloginfo('template_url'); ?>/images/thumbs/marketing.jpg" class="img-fluid" alt="Gestão de Marketing" />
+      <div class="col-12 col-md-4">
+        <a href="segmento/marketing-e-comercial" title="Gestão de Marketing" class="segmentos__card">
+          <div class="card-head">
+            <img src="<?php bloginfo('template_url'); ?>/images/thumbs/marketing.jpg" class="img-fluid" alt="Gestão de Marketing" />
+            <p>Crie fluxos e campanhas de e-mails automáticos. “Converse” com sua base a fim de nutri-los com conteúdos úteis e compartilháveis.</p>
+          </div>
           <h3>Marketing</h3>
-          <p>Crie fluxos e campanhas de e-mails automáticos. “Converse” com sua base a fim de nutri-los com conteúdos úteis e compartilháveis.</p>
         </a>
       </div>
 
-      <div class="col-12 col-md-4 segmentos__card">
-        <a href="segmento/marketing-e-comercial" title="Gestão de departamento comercial">
-          <img src="<?php bloginfo('template_url'); ?>/images/thumbs/comercial.jpg" class="img-fluid" alt="Gestão de departamento comercial" />
+      <div class="col-12 col-md-4">
+        <a href="segmento/marketing-e-comercial" title="Gestão de departamento comercial" class="segmentos__card">
+          <div class="card-head">
+            <img src="<?php bloginfo('template_url'); ?>/images/thumbs/comercial.jpg" class="img-fluid" alt="Gestão de departamento comercial" />
+            <p>Gerencie as vendas e obtenha relatórios com os principais indicadores do seu negócio. Com a gestão a vista identifique gaps e melhore seus resultados.</p>
+          </div>
           <h3>Comercial</h3>
-          <p>Gerencie as vendas e obtenha relatórios com os principais indicadores do seu negócio. Com a gestão a vista identifique gaps e melhore seus resultados.</p>
         </a>
       </div>
 
-      <div class="col-12 col-md-4 segmentos__card">
-        <a href="segmento/financeiro" title="Gestão de departamento financeiro">
-          <img src="<?php bloginfo('template_url'); ?>/images/thumbs/financeiro.jpg" class="img-fluid" alt="Gestão de departamento financeiro" />
+      <div class="col-12 col-md-4">
+        <a href="segmento/financeiro" title="Gestão de departamento financeiro" class="segmentos__card">
+          <div class="card-head">
+            <img src="<?php bloginfo('template_url'); ?>/images/thumbs/financeiro.jpg" class="img-fluid" alt="Gestão de departamento financeiro" />
+            <p>Automatize e controle todo o processo financeiro: alçadas de aprovações, contas a pagar, a receber, reembolso de despesas e muito mais!</p>
+          </div>
           <h3>Financeiro</h3>
-          <p>Automatize e controle todo o processo financeiro: alçadas de aprovações, contas a pagar, a receber, reembolso de despesas e muito mais!</p>
         </a>
       </div>
     </div>
@@ -160,7 +178,7 @@ require_once("header.php");
             </div>
           </div>
           */ ?>
-          
+
           <div class="tab-pane fade show active" id="v-pills-tarefas" role="tabpanel" aria-labelledby="v-pills-tarefas-tab" style="background: url(<?php bloginfo('template_url'); ?>/images/tabs/tarefas-prazos.jpg) no-repeat">
             <div class="tab-pane__content">
               <h3>Gestão de tarefas e prazos</h3>
---
+++ dist/wp-content/themes/abas/template_parts/segmentos-cards-old(backup).php
@@ -0,0 +1,74 @@
+<div class="row">
+  <div class="col-12 col-md-4 segmentos__card">
+    <a href="segmento/departamento-pessoal" title="Gestão de departamento pessoal">
+      <img src="<?php bloginfo('template_url'); ?>/images/thumbs/departamento-pessoal.jpg" class="img-fluid" alt="Gestão de departamento pessoal" />
+      <h3>Departamento Pessoal</h3>
+      <p>Personalize alertas e faça cálculo automático de férias por período de aquisição e substituição de funcionários. Calcule os benefícios: VT, VA e VR.</p>
+    </a>
+  </div>
+
+  <div class="col-12 col-md-4 segmentos__card">
+    <a href="segmento/recursos-humanos" title="Gestão de recursos humanos">
+      <img src="<?php bloginfo('template_url'); ?>/images/thumbs/recursos-humanos.jpg" class="img-fluid" alt="Gestão de recursos humanos" />
+      <h3>Recursos Humanos</h3>
+      <p>Facilite a seleção de candidatos: abertura e acompanhamento de vagas e envio automático de e-mails com o status da oportunidade.</p>
+    </a>
+  </div>
+
+  <div class="col-12 col-md-4 segmentos__card">
+    <a href="fluxo-de-trabalho/gestao-de-compras" title="Gestão de compras e estoque">
+      <img src="<?php bloginfo('template_url'); ?>/images/thumbs/compras-estoque.jpg" class="img-fluid" alt="Gestão de compras e estoque" />
+      <h3>Compras e Estoque</h3>
+      <p>Gerencie o processo de compras e estoque - desde o envio de e-mails ao fornecedor solicitando cotações até a baixa no estoque.</p>
+    </a>
+  </div>
+
+  <div class="col-12 col-md-4 segmentos__card">
+    <a href="segmento/manutencao" title="Gestão de manutenções">
+      <img src="<?php bloginfo('template_url'); ?>/images/thumbs/manutencao.jpg" class="img-fluid" alt="Gestão de manutenções" />
+      <h3>Manutenção</h3>
+      <p>Controle as manutenções que possuem periodicidade, receba alertas dos vencimentos e facilite a vistoria de acordo com as normas da ABNT.</p>
+    </a>
+  </div>
+
+  <div class="col-12 col-md-4 segmentos__card">
+    <a href="segmento/juridico" title="Gestão de setor jurídico">
+      <img src="<?php bloginfo('template_url'); ?>/images/thumbs/juridico.jpg" class="img-fluid" alt="Gestão de setor jurídico" />
+      <h3>Jurídico</h3>
+      <!-- <p>Rastreie dados pessoais e sensíveis, facilitando a elaboração do DPIA. Monitore os incidentes de segurança e a gestão de solicitação dos titulares.</p> -->
+      <p>Gerencie as apólices e contratos, faça anotações sobre o andamento de processos judiciais e emita alertas de vencimentos de forma automática.</p>
+    </a>
+  </div>
+
+  <div class="col-12 col-md-4 segmentos__card">
+    <a href="segmento/tecnologia-da-informacao" title="Gestão de demandas de TI">
+      <img src="<?php bloginfo('template_url'); ?>/images/thumbs/tecnologia-informacao.jpg" class="img-fluid" alt="Gestão de demandas de TI" />
+      <h3>Tecnologia da Informação</h3>
+      <p>Crie tarefas e faça a gestão de demandas do time de desenvolvimento. Aloque horas, acompanhe o andamento e envie status das atividades.</p>
+    </a>
+  </div>
+
+  <div class="col-12 col-md-4 segmentos__card">
+    <a href="segmento/marketing-e-comercial" title="Gestão de Marketing">
+      <img src="<?php bloginfo('template_url'); ?>/images/thumbs/marketing.jpg" class="img-fluid" alt="Gestão de Marketing" />
+      <h3>Marketing</h3>
+      <p>Crie fluxos e campanhas de e-mails automáticos. “Converse” com sua base a fim de nutri-los com conteúdos úteis e compartilháveis.</p>
+    </a>
+  </div>
+
+  <div class="col-12 col-md-4 segmentos__card">
+    <a href="segmento/marketing-e-comercial" title="Gestão de departamento comercial">
+      <img src="<?php bloginfo('template_url'); ?>/images/thumbs/comercial.jpg" class="img-fluid" alt="Gestão de departamento comercial" />
+      <h3>Comercial</h3>
+      <p>Gerencie as vendas e obtenha relatórios com os principais indicadores do seu negócio. Com a gestão a vista identifique gaps e melhore seus resultados.</p>
+    </a>
+  </div>
+
+  <div class="col-12 col-md-4 segmentos__card">
+    <a href="segmento/financeiro" title="Gestão de departamento financeiro">
+      <img src="<?php bloginfo('template_url'); ?>/images/thumbs/financeiro.jpg" class="img-fluid" alt="Gestão de departamento financeiro" />
+      <h3>Financeiro</h3>
+      <p>Automatize e controle todo o processo financeiro: alçadas de aprovações, contas a pagar, a receber, reembolso de despesas e muito mais!</p>
+    </a>
+  </div>
+</div>
