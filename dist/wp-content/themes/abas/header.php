6fb1cd85425fe4bf796e916033d1a4bbae981591
Carlos Eduardo Mayrinck Mendes <carlos.mendes@groupsoftware.com.br>
2022-09-19 10:40:07 -0300

    adicionando snippet google tag manager

--- dist/wp-content/themes/abas/header.php
+++ dist/wp-content/themes/abas/header.php
@@ -2,6 +2,25 @@
 <html lang="pt-br">
 
 <head>
+  <!-- Google Tag Manager -->
+  <script>
+    (function(w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({
+        'gtm.start': new Date().getTime(),
+        event: 'gtm.js'
+      });
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != 'dataLayer' ? '&l=' + l : '';
+      j.async = true;
+      j.src =
+        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+      f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-TMBC7CD');
+  </script>
+  <!-- End Google Tag Manager -->
+
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="shortcut icon" href="<?= get_template_directory_uri() . '/images/favicon.png' ?>" type="image/x-icon">
@@ -18,6 +37,10 @@
 </head>
 
 <body <?php body_class(); ?>>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TMBC7CD" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+
   <header>
     <div class="container">
       <nav class="navbar navbar-expand-lg navbar-light">
@@ -45,7 +68,7 @@
         ?>
 
         <!-- <a type="button" href="#" class="btn btn-amarelo d-none d-md-block" data-toggle="modal" data-target="#modalConversao">Solicitar demonstração <i class="fas fa-chevron-right"></i></a> -->
-        <a type="button" href="https://conteudo.abas.online/gerenciamento-de-tarefas" target="_blank" rel="noopener noreferrer" class="btn btn-amarelo d-none d-md-block" >Testar Grátis <i class="fas fa-chevron-right"></i></a>
+        <a type="button" href="https://conteudo.abas.online/gerenciamento-de-tarefas" target="_blank" rel="noopener noreferrer" class="btn btn-amarelo d-none d-md-block">Testar Grátis <i class="fas fa-chevron-right"></i></a>
       </nav>
     </div>
   </header>
